f3a22fd7f731059bcead311e522ca33c09e0a959
Claudio Bonati <claudio.bonati82@gmail.com>
2019-06-21 15:02:34 +0200

    multihistogram.py modified

--- multihistogram.py
+++ multihistogram.py
@@ -6,29 +6,21 @@ import scipy.misc as spm
 __all__ = ["multihisto_for_primary", "multihisto_for_secondary"]
 
 
-def _energy(vecdata):
-  """
-  Energy of a configuration carachterized by "vecdata"
-
-  for instance one could have in 4 dim:
-  vecdata = [average plaquette, average Polyakov loop]
-  energy  = 6*V*(1-vecdata[0])
-  """
-
-  return 6.0*8.0**4.0*(1.0-(vecdata[0]+vecdata[1])/2)
-
-
-def _newlzeta(lzeta, stuff, speedup):
+def newlzeta(energyfunc, lzeta, stuff, speedup):
   """
   The values of the partition functions to be computed are fixed points of this function
+  
+  energyfunc is the function to compute the 'energy'
 
-  Every element of stuff is a list of elements of the form
+  stuff is a list whose elements are of the form
   stuff[i]=[param_i, vecdata_i, blockdata_i]
   with vecdata=[column0, column1, .... ]
+
+  speedup >=1 is an integer
   """
  
   betas=np.array([element[0] for element in stuff])
-  stat=np.array([int(len(np.transpose(element[1]))/element[2]) for element in stuff])
+  stat=np.array([len(np.transpose(element[1])) for element in stuff])
 
   lzetanew=[]
 
@@ -37,10 +29,10 @@ def _newlzeta(lzeta, stuff, speedup):
     for element in stuff:
       end=element[2]*int(len(np.transpose(element[1]))/element[2])
   
-      energy=_energy(element[1][:end])
+      energy=energyfunc(element[1][:end])
 
-      tmp = np.array([ (-1)*spm.logsumexp(  np.array([ np.log(stat[j])-lzeta[j]+(betavalue-betas[j])*value for j in range(len(betas)) ]) ) for value in energy[0:end:speedup] ])
- 
+      tmp = np.array([ (-1)*spm.logsumexp(  np.sum(np.stack((np.log(stat), -lzeta, (betavalue-betas)*value), axis=1), axis=1) ) for value in energy[0:end:speedup] ])
+
       aux.append(spm.logsumexp(tmp))
 
     lzetanew.append(spm.logsumexp(np.array(aux)))
@@ -48,77 +40,86 @@ def _newlzeta(lzeta, stuff, speedup):
   return np.array(lzetanew)
 
 
-def _computelzeta(stuff):
+def computelzeta(energyfunc, stuff, *args):
   """
   Compute the log of the partition functions in a self-consistent way
 
-  Every element of stuff is a list of elements of the form
+  energyfunc is the function to compute the 'energy'
+
+  stuff is a list whose elements are of the form
   stuff[i]=[param_i, vecdata_i, blockdata_i]
   with vecdata=[column0, column1, .... ]
   """
 
-  speedup=int(min(np.array([len(np.transpose(element[1])) for element in stuff]))/10)
-  if(speedup<1):
+  speedup=100
+
+  if len(args)==0:
+    lzetaold=np.ones(len(stuff))
+  else:
+    lzetaold=np.copy(args[0])
     speedup=1
 
-  lzetaold=np.linspace(1, 2, len(stuff))
-
-  lzetanew=_newlzeta(lzetaold, stuff, speedup)
+  lzetanew=newlzeta(energyfunc, lzetaold, stuff, speedup)
   check=np.linalg.norm(lzetanew-lzetaold)
   lzetanew-=(lzetanew[0]-1)
+  check=np.linalg.norm(lzetanew-lzetaold)
 
   while speedup>1:
     lzetaold=np.copy(lzetanew)
-    lzetanew=_newlzeta(lzetaold, stuff, speedup)
+    lzetanew=newlzeta(energyfunc, lzetaold, stuff, speedup)
     lzetanew-=(lzetanew[0]-1)
     check=np.linalg.norm(lzetanew-lzetaold)
 
-    while check>10e-3:
+    while check>1e-2:
       lzetaold=np.copy(lzetanew)
-      lzetanew=_newlzeta(lzetaold, stuff, speedup)
+      lzetanew=newlzeta(energyfunc, lzetaold, stuff, speedup)
       lzetanew-=(lzetanew[0]-1)
       check=np.linalg.norm(lzetanew-lzetaold)
 
-      print("computing lzeta: {:.8g} ({:8d})".format(check, speedup), end='\r')
+      print("computing lzeta: {:12.8f} ({:8d})".format(check, speedup), end='\r')
 
     speedup=int(speedup/2)
     if speedup<1:
       speedup=1
 
-  while check>10e-5:
+  while check>1e-4:
     lzetaold=np.copy(lzetanew)
-    lzetanew=_newlzeta(lzetaold, stuff, speedup)
+    lzetanew=newlzeta(energyfunc, lzetaold, stuff, speedup)
     lzetanew-=(lzetanew[0]-1)
     check=np.linalg.norm(lzetanew-lzetaold)
-    print("computing lzeta: {:.8g} ({:8d})".format(check, speedup), end='\r' )
+    print("computing lzeta: {:12.8f} ({:8d})".format(check, speedup), end='\r' )
 
   return lzetanew
 
 
-def _jack_for_primary(func, param, lzeta, stuff):
+def jack_for_primary(func, param, energyfunc, lzeta, stuff):
   """
-  func is the function of the primaries to be comuted, 
+  func is the function of the primaries to be comuted,
+
+  param is the value of the control parameter ('beta')
+
+  energyfunc is the function to compute the 'energy'
+
   lzeta is the log of the partition functions 
-  and every element of stuff is a list of elements of the form
 
+  stuff is a list whose elements are of the form
   stuff[i]=[param_i, vecdata_i, blockdata_i]
-
   with vecdata=[column0, column1, .... ]
   """
 
   betas=np.array([element[0] for element in stuff])
-  stat=np.array([int(len(np.transpose(element[1]))/element[2]) for element in stuff])
-  numblocks=np.sum(stat)
+  stat=np.array([len(np.transpose(element[1])) for element in stuff])
+  numblocks=np.sum(np.array([int(len(np.transpose(element[1]))/element[2]) for element in stuff]) )
 
   auxZ=[]
   auxO=[]
   for element in stuff:
     end=element[2]*int(len(np.transpose(element[1]))/element[2])
   
-    energy=_energy(element[1])
+    energy=energyfunc(element[1])
     obs=func(element[1])
 
-    tmpZ = np.array([ (-1)*spm.logsumexp(  np.array([ np.log(stat[j])-lzeta[j]+(param-betas[j])*value for j in range(len(betas)) ]) ) for value in energy[:end] ])
+    tmpZ = np.array([ (-1)*spm.logsumexp(  np.sum(np.stack((np.log(stat), -lzeta, (param-betas)*value), axis=1), axis=1) ) for value in energy[:end] ])
     tmpO = tmpZ+np.log(obs[:end])
  
     auxZ = np.concatenate(( auxZ, spm.logsumexp( np.reshape(tmpZ,(-1, element[2])), axis=1) ))
@@ -135,17 +136,20 @@ def _jack_for_primary(func, param, lzeta, stuff):
   return np.exp(sumO-sumZ), np.std(jack)*np.sqrt(numblocks-1)
 
 
-def multihisto_for_primary(param_min, param_max, num_steps, func, *args):
+def multihisto_for_primary(param_min, param_max, num_steps, energyfunc, func, stuff):
   """
-  Compute the primary function func using multihistogram reweighting techniques with 
-  weight given by _energy for num_steps values of the control parameter, going from
-  param_min to param_max.
+  Compute the primary function func using multihistogram reweighting techniques
+  for num_steps values of the control parameter, going from param_min to
+  param_max.
 
-  Every element of *args is a list of two element of the form
-  args[i]=[param_i, vecdata_i, blockdata_i]
+  energyfunc is the function to compute the 'energy'
+
+  stuff is a list, whose elements of the form
+  stuff[i]=[param_i, vecdata_i, blockdata_i]
+  with vecdata=[column0, column1, .... ]
   """
 
-  for arg in args:
+  for arg in stuff:
     if not isinstance(arg[2], int):
       print("ERROR: blocksize has to be an integer!")
       sys.exit(1)
@@ -154,38 +158,34 @@ def multihisto_for_primary(param_min, param_max, num_steps, func, *args):
       print("ERROR: blocksize has to be positive!")
       sys.exit(1)
 
-  betas=np.array([element[0] for element in args])
-  stat=np.array([int(len(np.transpose(element[1]))/element[2]) for element in args])
-
-  print("Values of the control parameters: ", betas)
-  print("Statistics: ", stat)
-  print("")
-
-  lzeta=_computelzeta(args);
+  lzeta=computelzeta(energyfunc, stuff);
 
   print("log(zeta) = ", lzeta)
   print("")
 
   for i in range(num_steps+1):
     param=param_min+i*(param_max-param_min)/num_steps
-    ris, err = _jack_for_primary(func, param, lzeta, args) 
+    ris, err = jack_for_primary(func, param, energyfunc, lzeta, stuff) 
     print("{:.8f} {:.12g} {:.12g}".format(param, ris, err) )
 
 
-def _jack_for_secondary(func2, param, lzeta, stuff, vecfunc):
+def jack_for_secondary(func2, param, energyfunc, lzeta, vecfunc, stuff):
   """
   func2 is the function to be computed, 
+
+  param is the value of the control parameter ('beta')
+
+  energyfunc is the function to compute the 'energy'
+
   lzeta is the log of the partition functions 
-  and every element of stuff is a list of elements of the form
 
-  stuff[i]=[param_i, vecdata_i, blockdata_i]
-
-  with vecdata=[column0, column1, .... ] and
-
-  vecfunc=[func1a, func1b, func1c..... ]
-
+  vecfunc=[func1a, func1b, func1c..... ] 
   where func1a, func1b and so on are the functions of the
   primaries that are needed for func2.
+
+  stuff is a list whose elements are of the form
+  stuff[i]=[param_i, vecdata_i, blockdata_i]
+  with vecdata=[column0, column1, .... ]
   """
 
   # list of primary jackknife samples
@@ -195,8 +195,8 @@ def _jack_for_secondary(func2, param, lzeta, stuff, vecfunc):
   mean_list=[]
 
   betas=np.array([element[0] for element in stuff])
-  stat=np.array([int(len(np.transpose(element[1]))/element[2]) for element in stuff])
-  numblocks=np.sum(stat)
+  stat=np.array([len(np.transpose(element[1])) for element in stuff])
+  numblocks=np.sum(np.array([int(len(np.transpose(element[1]))/element[2]) for element in stuff]) )
 
   for func1 in vecfunc:
     auxZ=[]
@@ -204,10 +204,10 @@ def _jack_for_secondary(func2, param, lzeta, stuff, vecfunc):
     for element in stuff:
       end=element[2]*int(len(np.transpose(element[1]))/element[2])
     
-      energy=_energy(element[1])
+      energy=energyfunc(element[1])
       obs=func1(element[1])
   
-      tmpZ = np.array([ (-1)*spm.logsumexp(  np.array([ np.log(stat[j])-lzeta[j]+(param-betas[j])*value for j in range(len(betas)) ]) ) for value in energy[:end] ])
+      tmpZ = np.array([ (-1)*spm.logsumexp(  np.sum(np.stack((np.log(stat), -lzeta, (param-betas)*value), axis=1), axis=1) ) for value in energy[:end] ])
       tmpO = tmpZ+np.log(obs[:end])
    
       auxZ = np.concatenate(( auxZ, spm.logsumexp( np.reshape(tmpZ,(-1, element[2])), axis=1) ))
@@ -230,22 +230,22 @@ def _jack_for_secondary(func2, param, lzeta, stuff, vecfunc):
   return ris_no_bias, err
 
 
-def multihisto_for_secondary(param_min, param_max, num_steps, func2, vecfunc, *args):
+def multihisto_for_secondary(param_min, param_max, num_steps, func2, vecfunc, stuff):
   """
-  Compute the secondary function func2 using multihistogram reweighting techniques 
-  with weight given by _energy for num_steps values of the control parameter, going from
-  param_min to param_max.
-
-  Every element of *arg is a list of two element of the form
-  args[i]=[param_i, vecdata_i, blockdata_i]
+  Compute the secondary function func2 using multihistogram reweighting
+  techniques for num_steps values of the control parameter, going from param_min
+  to param_max.
 
   vecfunc=[func1a, func1b, func1c..... ]
   where func1a, func1b and so on are the functions of the
   primaries that are needed for func2.
 
+  stuff is a list whose elements are of the form
+  stuff[i]=[param_i, vecdata_i, blockdata_i]
+  with vecdata=[column0, column1, .... ]
   """
 
-  for arg in args:
+  for arg in stuff:
     if not isinstance(arg[2], int):
       print("ERROR: blocksize has to be an integer!")
       sys.exit(1)
@@ -254,21 +254,14 @@ def multihisto_for_secondary(param_min, param_max, num_steps, func2, vecfunc, *a
       print("ERROR: blocksize has to be positive!")
       sys.exit(1)
 
-  betas=np.array([element[0] for element in args])
-  stat=np.array([int(len(np.transpose(element[1]))/element[2]) for element in args])
-
-  print("Values of the control parameters: ", betas)
-  print("Statistics: ", stat)
-  print("")
-
-  lzeta=_computelzeta(args);
+  lzeta=computelzeta(energyfunc, stuff);
 
   print("log(zeta) = ", lzeta)
   print("")
 
   for i in range(num_steps+1):
     param=param_min+i*(param_max-param_min)/num_steps
-    ris, err = _jack_for_secondary(func2, param, lzeta, args, vecfunc) 
+    ris, err = jack_for_secondary(func2, param, lzeta, stuff, vecfunc) 
     print("{:.8f} {:.12g} {:.12g}".format(param, ris, err) )
 
 
@@ -284,27 +277,30 @@ if __name__=="__main__":
   print("UNIT TESTING")
   print()
 
-  #indata=np.loadtxt("dati_2.40.dat", skiprows=0, dtype=np.float)
-  #data240=np.transpose(indata)     #column ordered
+  #indata=np.loadtxt("dati_2.26.dat", skiprows=500, dtype=np.float)
+  #data226=np.transpose(indata)     #column ordered
 
-  #indata=np.loadtxt("dati_2.42.dat", skiprows=0, dtype=np.float)
-  #data242=np.transpose(indata)     #column ordered
+  #indata=np.loadtxt("dati_2.27.dat", skiprows=500, dtype=np.float)
+  #data227=np.transpose(indata)     #column ordered
 
-  #indata=np.loadtxt("dati_2.44.dat", skiprows=0, dtype=np.float)
-  #data244=np.transpose(indata)     #column ordered
+  #indata=np.loadtxt("dati_2.28.dat", skiprows=500, dtype=np.float)
+  #data228=np.transpose(indata)     #column ordered
+
+  #def energy(vec):
+  #  return 6*4*8**3*(1-(vec[0]+vec[1])/2)
 
   #def plaq(vec):
-  #  return (vec[0]+vec[1])/2
+  #  return (vec[0]+vec[1])/2.0
 
   #def squareplaq(vec):
-  #  return (vec[0]+vec[1])*(vec[0]+vec[1])/4
+  #  return (vec[0]+vec[1])*(vec[0]+vec[1])/4.0
 
-  #def plaqsusc(v):
+  #def susc(v):
   #  return v[0]-v[1]*v[1]
 
-  #multihisto_for_primary(2.40, 2.44, 200, plaq, [2.40, data240, 1], [2.42, data242, 1], [2.44, data244, 1])
+  #stuff=[[2.26, data226, 20], [2.27, data227, 20], [2.28, data228, 100]]
 
-  #multihisto_for_secondary(2.40, 2.44, 20, plaqsusc, [squareplaq, plaq], [2.40, data240, 10], [2.42, data242, 10], [2.44, data244, 10])
+  #multihisto_for_secondary(2.26, 2.28, 20, susc, [squareplaq, plaq], stuff)
 
   print("**********************")
 
